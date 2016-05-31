a2454b98aa3c678532bc94cf6ba24ae4282d7abf
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-05-31 18:15:24 +0300

    Restructure files, Map main functions flow

---
+++ src/inverted-index.js
@@ -0,0 +1,25 @@
+//Load JSON from local file, use relative indexing
+
+
+
+//Loop through JSON array, getting each document
+
+
+
+//Make string of all keys in each object in JSON array and lowercase string
+
+
+
+//Remove all stop words from lowercase string
+
+
+
+//Loop though string while splitting on space and Create index of words in string
+//({term:[1,3],[2,6]},{term2:[1,0],[2,0],[3,3]}})
+//See about outputting pretty table
+
+
+//For function-search (terms) filter index for search terms
+//Return object ({term:[1,3],[2,6]},{term2:[1,0],[2,0],[3,3]}})
+//Make it clearer---- term + "in Doc" + varname.key[0][0] + "appears" + varname.key[0][1] + "times"
+
