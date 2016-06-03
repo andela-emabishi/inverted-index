fbcd279e7451e104c330af866c5414ba43025b2c
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-06-03 20:14:55 +0300

    Accept all JSONs

--- jasmine/src/inverted-index.js
+++ jasmine/src/inverted-index.js
@@ -2,11 +2,11 @@
 
 function Index(){
 	this.createIndex = function(fileName){
-		var json_file = require("../" + fileName + ".json");
-		    //console.log(json_file);
+		var jsonFile = require("../" + fileName + ".json");
+		    //console.log(jsonFile);
 	
 	this.getIndex = function(){
-		json_file.map((book, docIndex) => {
+		jsonFile.map((book, docIndex) => {
 	var wordsArray = [];
 	var books = JSON.stringify(book).toLowerCase().split(/\W/g).filter(function (string_){
 		return string_.length !== 0;});
@@ -22,24 +22,28 @@ function Index(){
 	this.searchIndex = function(terms){
 	var results = [];
 
-	json_file.map((book, index) => {
+	jsonFile.map((book, index) => {
 		const wordToSearch = new RegExp(terms, 'gi');
-		if (wordToSearch.test(book.title) || wordToSearch.test(book.text)) {
+		var books = JSON.stringify(book).toLowerCase().split(/\W/g).filter(function (string_){
+		return string_.length != 0;});
+		if (wordToSearch.test(books) || wordToSearch.test(books)) {
 			results.push(index); 
 		}
 	});
 
 	if(results.length > 0) {
-		console.log(terms + " has been found in the following documents" + " " + results)
+		console.log(terms + " has been found in the following documents:" + " " + results)
 	} else{
 		console.log("No match has been made")
 	}
 	};
 };
 }
-var index_ = new Index();
-index_.createIndex('books');
-index_.searchIndex('lord');
+
+var index = new Index();
+index.createIndex('books');
+index.searchIndex('title');
+index.getIndex();
 
 
 
