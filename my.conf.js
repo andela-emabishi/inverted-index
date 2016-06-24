67e6bfe2ba22acc1b608ccf0c1d3ee2fa12929a3
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-06-24 19:30:53 +0300

    Add travis config file

--- my.conf.js
+++ my.conf.js
@@ -15,8 +15,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '/jasmine/spec/inverted-index-spec.js',
-      '/jasmine/src/inverted-index.js'
+      'jasmine/spec/*.js',
+      'jasmine/src/*.js'
     ],
 
 
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Firefox'],
 
 
     // Continuous Integration mode
