0f7fc6fc64d8fc865999f86b4ff21e6be3275470
Elizabeth Mabishi <elizabeth.wakio@andela.com>
2016-06-24 18:47:35 +0300

    Add Karma config file

--- my.conf.js
+++ my.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration
-// Generated on Fri Jun 24 2016 16:51:00 GMT+0300 (EAT)
+// Generated on Fri Jun 24 2016 18:22:12 GMT+0300 (EAT)
 
 module.exports = function(config) {
   config.set({
@@ -10,13 +10,13 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'requirejs'],
+    frameworks: ['jasmine'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      {pattern: 'jasmine/spec/inverted-index-spec.js', included: false},
-      {pattern: 'jasmine/src/inverted-index.js', included: false}
+      '/jasmine/spec/inverted-index-spec.js',
+      '/jasmine/src/inverted-index.js'
     ],
 
 
@@ -51,17 +51,17 @@ module.exports = function(config) {
 
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+    autoWatch: false,
 
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari', 'ChromeCanary'],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+    singleRun: false,
 
     // Concurrency level
     // how many browser should be started simultaneous
